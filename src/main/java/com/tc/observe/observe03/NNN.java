63f4e8f065bd374dfaca0815e190dd0367a9c1aa
tianchao <455433520@qq.com>
2019-10-16 13:42:25 +0800

    test

---
+++ src/main/java/com/tc/observe/observe03/NNN.java
@@ -0,0 +1,18 @@
+package com.tc.observe.observe03;
+
+/**
+ * Description:
+ * <p>
+ * ModelName:【】模块
+ *
+ * @author: TianChao
+ * Create at:  2019/10/16 10:30
+ * Company: 沈阳艾尔时代科技发展有限公司
+ * Copyright: (c)2018 AIR Times Inc. All rights reserved.
+ * @version: 1.0
+ */
+public class NNN {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
