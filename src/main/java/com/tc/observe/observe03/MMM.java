7a1f5e25989f997a8bb87480ed80a0e5ff0bffc8
tianchao <455433520@qq.com>
2019-10-16 10:28:21 +0800

    insert

---
+++ src/main/java/com/tc/observe/observe03/MMM.java
@@ -0,0 +1,15 @@
+package com.tc.observe.observe03;
+
+/**
+ * Description:
+ * <p>
+ * ModelName:【】模块
+ *
+ * @author: TianChao
+ * Create at:  2019/10/16 10:26
+ * Company: 沈阳艾尔时代科技发展有限公司
+ * Copyright: (c)2018 AIR Times Inc. All rights reserved.
+ * @version: 1.0
+ */
+public class MMM {
+}
